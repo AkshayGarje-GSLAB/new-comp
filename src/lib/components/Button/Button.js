fa4acb1b1fb91d0eafc543a84ff45864b0b71971
Akshay Garje <akshay.garje@gslab.com>
2022-07-22 01:39:44 +0530

    chnage

--- src/lib/components/Button/Button.js
+++ src/lib/components/Button/Button.js
@@ -1,6 +1,5 @@
 import React from "react";
-import MuiButton from "@mui/material/Button";
 
 export default Button = (props) => {
-  return <MuiButton {...props}>{props.children}</MuiButton>;
+  return <div>OK</div>;
 };
